ffbdb4f962b0db4e7ffa76e055d625995a98f457
Mia <mia.krause004@stud.fh-dortmund.de>
2018-07-06 09:38:12 +0200

    [stable] (but ugly) Random Card Selection

--- app/src/main/java/com/example/fabiandrees/list/Flashcard.java
+++ app/src/main/java/com/example/fabiandrees/list/Flashcard.java
@@ -1,14 +1,18 @@
 package com.example.fabiandrees.list;
 
-public class Flashcard {
+import java.io.Serializable;
+
+public class Flashcard implements Serializable {
     private String topic;
     private String text;
     private String title;
+    private int level;
 
     public Flashcard(String topic, String title, String text) {
         this.topic = topic;
         this.title = title;
         this.text = text;
+        this.level = 0;
     }
 
     public String getTopic() {
@@ -34,4 +38,12 @@ public class Flashcard {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public int getLevel() { return level; }
+
+    public void setLevel(int level) { this.level = level; }
+
+    public void incrementLevel(){ level++; }
+
+    public void decrementLevel(){ level--; }
 }
